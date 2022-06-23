175fe3e2abe8063ae5406de758d5b8aa31a67bb4
Eric Ahn <erah@microsoft.com>
2022-06-23 00:19:23 -0700

    threads

---
+++ Application.cpp
@@ -0,0 +1,98 @@
+/****************************************************************************
+ * Copyright (C) 2015 Dimok
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
+#include "Application.h"
+//#include "common/common.h"
+//#include "dynamic_libs/os_functions.h"
+//#include "dynamic_libs/sys_functions.h"
+//#include "gui/FreeTypeGX.h"
+//#include "gui/DVPadController.h"
+//#include "gui/DWPadController.h"
+//#include "resources/Resources.h"
+//#include "sounds/SoundHandler.hpp"
+//#include "utils/logger.h"
+#include <vpad/input.h>
+#include <whb/log.h>
+#include "log_console.h"
+
+Application *Application::applicationInstance = NULL;
+bool Application::exitApplication = false;
+
+Application::Application()
+	: CThread(CThread::eAttributeAffCore1 | CThread::eAttributePinnedAff, 0, 0x20000)
+    , exitCode(0)
+{
+	exitApplication = false;
+}
+
+Application::~Application()
+{
+}
+
+int Application::exec()
+{
+    //! start main thread
+    resumeThread();
+    //! now wait for thread to finish
+	shutdownThread();
+
+	return exitCode;
+}
+
+void Application::executeThread(void)
+{
+    VPADStatus status;
+    VPADReadError error;
+    while (!exitApplication) {
+        // Read button, touch and sensor data from the Gamepad
+        VPADRead(VPAD_CHAN_0, &status, 1, &error);
+        switch (error) {
+            case VPAD_READ_SUCCESS: {
+                // No error
+                break;
+            }
+            case VPAD_READ_NO_SAMPLES: {
+                // No new sample data available
+                continue;
+            }
+            case VPAD_READ_INVALID_CONTROLLER: {
+                // Invalid controller
+                continue;
+            }
+            /*
+            case VPAD_READ_BUSY: {
+                // Busy
+                continue;
+            }
+            */
+            default: {
+                if ((int32_t) error == -4) {
+                    continue;
+                }
+                WHBLogPrintf("Unknown VPAD error! %08X", error);
+                WHBLogConsoleDraw();
+                break;
+            }
+        }
+
+        if (status.trigger & VPAD_BUTTON_B) {
+            exitApplication = true;
+        }
+        else if (status.trigger & VPAD_BUTTON_HOME) {
+            exitApplication = true;
+        }
+    }
+}
--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <vpad/input.h>
 #include <whb/log.h>
 #include "log_console.h"
+#include "Application.h"
 
 #include <whb/proc.h>
 
@@ -43,50 +44,11 @@ int main(int argc, char** argv)
     //!*******************************************************************
     WHBLogPrint("Start main application");
     WHBLogConsoleDraw();
-    //DEBUG_FUNCTION_LINE("Start main application\n");
-    //int returnCode = Application::instance()->exec();
-    VPADStatus status;
-    VPADReadError error;
-    while (WHBProcIsRunning()) {
-        /*  Read button, touch and sensor data from the Gamepad */
-        VPADRead(VPAD_CHAN_0, &status, 1, &error);
-        switch (error) {
-            case VPAD_READ_SUCCESS: {
-                // No error
-                break;
-            }
-            case VPAD_READ_NO_SAMPLES: {
-                // No new sample data available
-                continue;
-            }
-            case VPAD_READ_INVALID_CONTROLLER: {
-                // Invalid controller
-                continue;
-            }
-            /*
-            case VPAD_READ_BUSY: {
-                // Busy
-                continue;
-            }
-            */
-            default: {
-                if ((int32_t) error == -4) {
-                    continue;
-                }
-                WHBLogPrintf("Unknown VPAD error! %08X", error);
-                break;
-            }
-        }
-
-        if (status.trigger & VPAD_BUTTON_B) {
-            break;
-        }
-        WHBLogConsoleDraw();
-    }
+    int returnCode = Application::instance()->exec();
 
     //DEBUG_FUNCTION_LINE("Main application stopped\n");
 
-    //Application::destroyInstance();
+    Application::destroyInstance();
 
     WHBLogPrint("Unmount SD");
     WHBLogConsoleDraw();
@@ -98,5 +60,5 @@ int main(int argc, char** argv)
 
     WHBLogConsoleFree();
     WHBProcShutdown();
-    return 0;
+    return returnCode;
 }
